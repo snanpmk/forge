f5c0a568f6bcf6b45a8da0652bafa0baa226739c
Sinan Pmk <sinan@finnest.io>
2025-12-13 08:15:34 +0530

    refactor: use regex pattern for catch-all route handler

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // Any other route loads index.html
-  app.get('*', (req, res) =>
+  app.get(/(.*)/, (req, res) =>
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
   );
 } else {
